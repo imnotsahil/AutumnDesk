33c09d83c0127c321562e56be6743157e0042733
sahil <imnotsahil@gmail.com>
2019-11-08 21:44:19 +0530

    secondcommit

---
+++ Resources.php
@@ -0,0 +1,7 @@
+<?php 
+    $name='your name';
+    echo '<table>
+       <tr><th>Name</th></tr>
+       <tr><td><a href="https://www.w3schools.com/bootstrap/tryit.asp?filename=trybs_ref_glyph_education&stacked=h">Click</a></td></tr>
+    </table>';
+?>
