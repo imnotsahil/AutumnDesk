a149faab0c449799f74eb8eeb37cbaad688e3ec6
imnotsahil <47858118+imnotsahil@users.noreply.github.com>
2019-11-19 12:47:35 +0530

    Update feed.php

--- feed.php
+++ feed.php
@@ -6,6 +6,7 @@ $email = $_POST['email'];
 $subject = $_POST['subject'];
 $id=uniqid();
 $date=date("Y-m-d");
+date_default_timezone_set('Asia/Kolkata');
 $time=date("h:i:sa");
 $feedback = $_POST['feedback'];
 $q=mysqli_query($con,"INSERT INTO feedback VALUES  ('$id' , '$name', '$email' , '$subject', '$feedback' , '$date' , '$time')")or die ("Error");
